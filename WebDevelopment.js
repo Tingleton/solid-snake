48c0db9438ac742078d8dfa2fa2ffa426a09ccdf
Tingleton <34091809+Tingleton@users.noreply.github.com>
2019-01-13 23:22:21 +0000

    Add files via upload

---
+++ WebDevelopment.js
@@ -0,0 +1,7 @@
+/* 
+HTML5 Responsive Template
+Simple Responsive Web Design With CSS3 Media Queries
+SOURCE: <a href="http://onlinewebapplication.com/simple-responsive-web-design-with-css3-media-queries/" target="_blank"><i><b>Responsive HTML5 Page Using Media Queries</b></i></a>
+http://onlinewebapplication.com/demos/responsive-web-design/
+ by onlinewebapplication.com
+ */
